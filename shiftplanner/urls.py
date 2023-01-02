36d324def49293c9550df40d4c97875af80b2bb6
naiyoma <lankas.aurelia@gmail.com>
2023-01-02 22:05:37 +0300

    virtual env

--- shiftplanner/urls.py
+++ shiftplanner/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from allauth.account.views import confirm_email
-from django.conf.urls import url
 
 
 urlpatterns = [
--- users/models.py
+++ users/models.py
@@ -1,11 +1,11 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils.translation import ugettext_lazy as _
-
 from .managers import CustomUserManager
 
 
 class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(verbose_name="name", max_length=60, unique=True)
     username = models.CharField(max_length=30, unique=True)
     date_joined = models.DateField(verbose_name='date joined', auto_now_add=True)
