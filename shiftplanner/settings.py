cc2c9323c9544a98d423480b43c884586f31dfbc
naiyoma <lankas.aurelia@gmail.com>
2023-02-24 15:50:41 +0300

    docs: update

--- shiftplanner/settings.py
+++ shiftplanner/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-load_dotenv(BASE_DIR / '.env')
+# load_dotenv(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
